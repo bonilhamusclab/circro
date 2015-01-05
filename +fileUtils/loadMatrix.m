0fdf432c944eb67e4dad2291bb45adf84b8f0e9d
John <*>
2015-01-05 14:57:18 -0500

    slight simplification

---
+++ +fileUtils/loadMatrix.m
@@ -0,0 +1,3 @@
+function matrix = loadMatrix(matrix_fullpath)
+%function matrix = loadMatrix(matrix_fullpath)
+matrix = xlsread(matrix_fullpath);
--- +gui/Add_matrix_Callback.m
+++ +gui/Add_matrix_Callback.m
@@ -1,23 +1,11 @@
 % --- Add matrix
 function Add_matrix_Callback(obj, ~)
 v=guidata(obj);
-Select_matrix_data(v);
-v=guidata(obj);
-matrix=v.matrix;
-number_links=size(matrix,1);
-% check if list regions previously defined
-if isfield(v,'list_regions')
-    list_regions=v.list_regions; % nodes were previously selected
-else
-    temp=[(1:number_links/2)' flipud((((number_links/2)+1):number_links)')]; % nodes were not previously selected- assigning names
-    list_regions=(num2cell(temp));
-end
-% check if sizes previously defined
-if isfield(v,'node_sizes')
-    node_sizes=v.list_regions; % nodes were previously selected
-else
-    temp=[(1:num_value_colors/2)' flipud((((num_value_colors/2)+1):num_value_colors)')]; % nodes were not previously selected- assigning names
-    list_regions=(num2cell(temp));
-end
 
-drawing.draw_links(matrix,threshold,start_radian,radius)
+[matrix_filename, matrix_pathname] = uigetfile( ...
+                    {'*.xlsx;', 'excel files (*.xlsx)';...
+                    '*.xls;', 'excel files (*.xls)'; ...
+                    '*.*',       'All Files (*.*)'},...
+                    'Select a text file');
+if isequal(matrix_filename,0), return; end;
+commands.addMatrix(v, [matrix_pathname matrix_filename]);
