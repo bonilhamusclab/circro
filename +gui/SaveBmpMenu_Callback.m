197c879e430ab92b42d272f418b57ee7a5255ccc
John <*>
2015-01-05 17:01:39 -0500

    only Circo at root

--- +gui/SaveBmpMenu_Callback.m
+++ +gui/SaveBmpMenu_Callback.m
@@ -3,5 +3,5 @@ function SaveBmpMenu_Callback(obj, ~)
 v=guidata(obj);
 [file,path] = uiputfile('*.png','Save image as');
 if isequal(file,0), return; end;
-saveBitmap(v,[path file]);
+commands.saveBitmap(v,[path file]);
 %end SaveBmpMenu_Callback()
