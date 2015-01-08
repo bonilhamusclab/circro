836a502489986bc2df827ae69d158b150d2bd3a6
LB User <joftheancientgermanspear@users.noreply.github.com>
2015-01-08 17:54:19 -0500

    file selection drawing

---
+++ +gui/+circularDiagram/filesSelection.m
@@ -0,0 +1,150 @@
+function varargout = filesSelection(varargin)
+% FILESSELECTION MATLAB code for filesSelection.fig
+%      FILESSELECTION, by itself, creates a new FILESSELECTION or raises the existing
+%      singleton*.
+%
+%      H = FILESSELECTION returns the handle to a new FILESSELECTION or the handle to
+%      the existing singleton*.
+%
+%      FILESSELECTION('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in FILESSELECTION.M with the given input arguments.
+%
+%      FILESSELECTION('Property','Value',...) creates a new FILESSELECTION or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before filesSelection_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to filesSelection_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help filesSelection
+
+% Last Modified by GUIDE v2.5 08-Jan-2015 12:25:56
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       '+gui/+circularDiagram/filesSelection.fig', ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @filesSelection_OpeningFcn, ...
+                   'gui_OutputFcn',  @filesSelection_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before filesSelection is made visible.
+function filesSelection_OpeningFcn(hObject, ~, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to filesSelection (see VARARGIN)
+
+% Choose default command line output for filesSelection
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes filesSelection wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+function varargout = filesSelection_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+% --- Executes on button press in selectSizesFile_button.
+function selectLabelsFile_button_Callback(hObject, eventdata, handles)
+% hObject    handle to selectSizesFile_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% --- Executes on button press in selectSizesFile_button.
+function selectSizesFile_button_Callback(hObject, eventdata, handles)
+% hObject    handle to selectSizesFile_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in selectEdgeMatrixFile_button.
+function selectEdgeMatrixFile_button_Callback(hObject, eventdata, handles)
+% hObject    handle to selectEdgeMatrixFile_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+% --- Executes on button press in selectColorsFile_button.
+function selectColorsFile_button_Callback(hObject, eventdata, handles)
+% hObject    handle to selectColorsFile_button (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+% --- Executes during object creation, after setting all properties.
+function labelsFilePath_txtbox_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sizesFilePath_txtbox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function sizesFilePath_txtbox_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sizesFilePath_txtbox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+% --- Executes during object creation, after setting all properties.
+function colorsFilePath_txtbox_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to colorsFilePath_txtbox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+% --- Executes during object creation, after setting all properties.
+function edgeMatrxPath_txt_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edgeMatrxPath_txt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
--- +gui/AddCircularDiagram_Callback.m
+++ +gui/AddCircularDiagram_Callback.m
@@ -1,10 +1,13 @@
 % Callback functions
 % --- add a circle with default options
 function AddCircularDiagram_Callback(obj, ~)
+%{
 [list_filename, list_pathname] = uigetfile( ...
                     {'*.xlsx;', 'excel files (*.xlsx)';...
                     '*.xls;', 'excel files (*.xls)'; ...
                     '*.*',       'All Files (*.*)'},...
                     'Select an excel file');
 if isequal(list_filename,0), return; end;
+%}
+uiwait(gui.circularDiagram.filesSelection);
 commands.addCircularDiagram(guidata(obj), [list_pathname list_filename]);
