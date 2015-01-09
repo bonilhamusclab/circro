dd19d144d502f94a53ee78c1b8c7e1008ab989f3
LB User <joftheancientgermanspear@users.noreply.github.com>
2015-01-09 16:22:19 -0500

    drawing matrix too

--- +commands/setCircularEdgeMatrix.m
+++ +commands/setCircularEdgeMatrix.m
@@ -1,23 +1,27 @@
-function setCircularEdgeMatrix(v, matrix_full_path, threshold, start_radian, radius)
-matrix = fileUtils.loadMatrix(matrix_full_path);
-v.matrix=matrix;     
+function setCircularEdgeMatrix(v, edgeMatrixFullPath, varargin)
+matrix = fileUtils.loadMatrix(edgeMatrixFullPath);
+v.matrix=matrix;
 
-% number_links=size(matrix,1);
-% % check if list regions previously defined
-% if isfield(v,'list_regions')
-%     list_regions=v.list_regions; % nodes were previously selected
-% else
-%     temp=[(1:number_links/2)' flipud((((number_links/2)+1):number_links)')]; % nodes were not previously selected- assigning names
-%     list_regions=(num2cell(temp));
-% end
-% % check if sizes previously defined
-% if isfield(v,'node_sizes')
-%     node_sizes=v.list_regions; % nodes were previously selected
-% else
-%     temp=[(1:num_value_colors/2)' flipud((((num_value_colors/2)+1):num_value_colors)')]; % nodes were not previously selected- assigning names
-%     list_regions=(num2cell(temp));
-% end
+inputs = parseInputParamsSub(varargin);
+threshold = inputs.threshold;
+startRadian = inputs.startRadian;
+radius = inputs.radius;
 
 
 guidata(v.hMainFigure,v);
-drawing.draw_links(matrix,threshold,start_radian,radius);
+drawing.draw_links(matrix,threshold,startRadian,radius);
+
+function inputParams = parseInputParamsSub(args)
+p = inputParser;
+d.threshold = .5; d.startRadian = 0; d.radius = 1;
+
+validateChar = @(x) validateattributes(x, {'char'}, {});
+
+p.addOptional('threshold', d.threshold, validateChar);
+p.addOptional('startRadian', d.startRadian, validateChar);
+p.addOptional('radius', d.radius, validateChar);
+
+p = utils.stringSafeParse(p, args, fieldnames(d), ...
+    d.threshold, d.startRadian, d.radius);
+
+inputParams = p.Results;
