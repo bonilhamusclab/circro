a5112e01344c06ff825bf630bcc4503dc46ae6f7
LB User <joftheancientgermanspear@users.noreply.github.com>
2015-01-12 13:27:41 -0500

    setting node sizes keeps labels

--- +commands/setCircularNodeSizes.m
+++ +commands/setCircularNodeSizes.m
@@ -3,7 +3,7 @@ nodeSizes = fileUtils.loadSizes(sizesFullPath);
 v.nodeSizes=nodeSizes;
 numNodeSize=numel(nodeSizes);
 nodeSizes(1:numNodeSize/2,2)=flipud(((numNodeSize/2)+1:numNodeSize)'); % here in case I decide to add display values in the future
-if isfield(v,'lables')
+if isfield(v,'labels')
     labels=v.labels; % nodes were previously selected
 else
     temp=[(1:numNodeSize/2)' flipud((((numNodeSize/2)+1):numNodeSize)')]; % nodes were not previously selected- assigning names
