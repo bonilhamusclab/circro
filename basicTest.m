d11b90dfeaf8eeb9c87b2384304e964958d076d7
LB User <joftheancientgermanspear@users.noreply.github.com>
2015-06-05 11:27:03 -0400

    add basic test

---
+++ basicTest.m
@@ -0,0 +1,4 @@
+Circro('circro.setEdgeMatrix','testData/matrix.xlsx',1.6,'circleIndex',1);
+Circro('circro.setNodeLabels','testData/labels.xlsx',1);
+Circro('circro.setNodeColors','testData/colors.xlsx','hot',0.8,1);
+Circro('circro.setNodeSizes','testData/sizes.xlsx',1);
